d629993da40d6c6ae43a2c37fe508f97aa695d48
Zan-Sajuela <85975677+Zan-Sajuela@users.noreply.github.com>
2021-07-13 11:03:33 +0800

    Added axios in packagejson

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,90 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
+import axios from 'axios';
 
-export default function Register(){
+class Register extends Component{
 
-   
+  state = {
+    fname:'',
+    lname:'',
+    email:'',
+    password:''
+  }
 
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]:e.target.value
+    });
+  }
+
+  saveRegister = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('http://localhost:8000/api/register', this.state);
+    alert("alert")
+  }
+
+render(){
 return(
-    <Form>
+    <Form onSubmit={this.saveRegister}>
         <Form.Group controlId="formFile" className="mb-3">
             <Form.Label>Default file input example</Form.Label>
             <Form.Control type="file" />
         </Form.Group>
-
-        
         <Form.Group controlId="formBasicPassword">
-
               <Form.Label>First Name</Form.Label>
-                <Form.Control 
-                type="name" 
-                placeholder="first name" />
+                <Form.Control  type="text" name="fname" value={this.state.fname} onChange={this.handleInput}/>
         </Form.Group>
-   
-
-
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Last Name</Form.Label>
-        <Form.Control 
-        type="name" 
-        placeholder="last-name" />
+                <Form.Control type="text" name="lname" value={this.state.lname} onChange={this.handleInput} />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
       <Form.Label>Email</Form.Label>
-      <Form.Control 
-      type="Email" 
-      placeholder="Email" />
+            <Form.Control type="text" name="email" value={this.state.email} onChange={this.handleInput} />
       <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
     </Form.Group>
-
-
-        
-        
-     
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control 
-        type="password" 
-        placeholder="Password" />
+        <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleInput} />
       </Form.Group>
 
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Verify Password</Form.Label>
-        <Form.Control 
-            type="password" 
-            placeholder="verify password"  />
-      </Form.Group>
 
-    
-
-      
-    <Form.Group as={Col} controlId="formGridState">
-    <Form.Label>Gender</Form.Label>
-    <Form.Control as="select" defaultValue="Choose...">
-      <option>Male</option>
-      <option>Female</option>
-    </Form.Control>
-  </Form.Group>
-
-  <Form.Group as={Col} controlId="formGridState">
-    <Form.Label>Language</Form.Label>
-    <Form.Control as="select" defaultValue="Choose...">
-      <option>Japanese</option>
-      <option>English</option>
-      <option>Bisaya</option>
-    </Form.Control>
-  </Form.Group>
-
-
-      <Button  type="submit" > 
-        Register
-      </Button>
+      <Button  type="submit" >Register</Button>
     </Form>
     )
+  }
 }
+export default Register;
+ //   <Form.Group as={Col} controlId="formGridState">
+  //   <Form.Label>Gender</Form.Label>
+  //   <Form.Control as="select" defaultValue="Choose...">
+  //     <option>Male</option>
+  //     <option>Female</option>
+  //   </Form.Control>
+  // </Form.Group>
+
+  // <Form.Group as={Col} controlId="formGridState">
+  //   <Form.Label>Language</Form.Label>
+  //   <Form.Control as="select" defaultValue="Choose...">
+  //     <option>Japanese</option>
+  //     <option>English</option>
+  //     <option>Bisaya</option>
+  //   </Form.Control>
+  // </Form.Group>
