90d67620afd6698a05edbe8f54bbf823d62eb2da
Zan-Sajuela <85975677+Zan-Sajuela@users.noreply.github.com>
2021-07-13 11:30:11 +0800

    Modified indentions on each js files make sure it has 2 space indentions

--- frontend/src/components/Navibar.js
+++ frontend/src/components/Navibar.js
@@ -8,25 +8,25 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NaviBar() {
 
-	return (
-		<Navbar>
-            <Container>
-                <Navbar.Brand href="/">E-Learning</Navbar.Brand>
-                    <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                  <Nav className="me-auto">
-                      <Nav.Link href="/login">Login</Nav.Link>
-                      <Nav.Link href="/register">Register</Nav.Link>
-                      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Lessons</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Categories</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Profiles</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Admin</NavDropdown.Item>
-                      </NavDropdown>
-                    </Nav>  
-                </Navbar.Collapse>
-            </Container>
-     </Navbar>
+return (
+  <Navbar>
+    <Container>
+      <Navbar.Brand href="/">E-Learning</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav className="me-auto">
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/register">Register</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Lessons</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Categories</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Profiles</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Admin</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>  
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 	)
 }
