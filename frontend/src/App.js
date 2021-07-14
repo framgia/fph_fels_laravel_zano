b8bf0ee30a73d509f14ba5c72b3c6e64fe752e5d
Zan-Sajuela <85975677+Zan-Sajuela@users.noreply.github.com>
2021-07-14 14:51:08 +0800

    Made Changes to the indentions of the return

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,19 +9,19 @@ import Home from './pages/Home';
 
 function App() {
 
-return(
-  <div className="App">
-    <BrowserRouter>
-      <NaviBar />
-      <Container className="my-3">		
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </Container>
-    </BrowserRouter>
-  </div>
+  return(
+    <div className="App">
+      <BrowserRouter>
+        <NaviBar />
+        <Container className="my-3">		
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 export default function Home() {
   
-return (
-  <React.Fragment>
-     <h1>Home Page</h1>
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <h1>Home Page</h1>
+    </React.Fragment>
   )
 }
